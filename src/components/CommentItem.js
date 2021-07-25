b679e8496f69b92583f20d281b50eeca2a69e958
Skrekliam <typnickman@gmail.com>
2021-07-25 14:17:27 +0300

    Add comments read from db

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function CommentItem() {
+function CommentItem({item}) {
+    console.log(item)
   return (
     <div className="comment">
-      <h4>Lorem.</h4>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <span>{new Date().toDateString()}</span>
+      <h4>{item.data.author }</h4>
+      <p>{item.data.text }</p>
+      <span>{new Date(item.data.time.seconds*1000).toDateString()}</span>
       <hr />
     </div>
   );
--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -9,51 +9,70 @@ import "./InfoPage.css";
 function InfoPage() {
   let { id } = useParams();
   const [item, setItem] = useState();
-
+  const [comments, setComments] = useState([]);
   useEffect(() => {
     db.collection("products")
       .doc(id)
       .get()
       .then((doc) => {
-          console.log(doc)
+        console.log(doc);
         if (doc.exists) setItem({ id: doc.id, product: doc.data() });
-      }).catch(err => console.log(err));
-  }, []);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
 
-  console.log(item,id)
+  useEffect(() => {
+    if (!item) return;
+    db.collection("products")
+      .doc(id)
+      .collection("comments")
+      .orderBy("time", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+    }, [item]);
+    
+    console.log('comments', comments)
+  console.log(item, id);
   return (
-    <div className="infoPage">{item ?(<>
-      <div className="head">
-        <div className="image">
-          <Paper elevation={3}>
-            <img
-              src={item.product.url}
-              alt={item.product.name}
-            />
-          </Paper>
-        </div>
-        <Paper elevation={3}>
-          <div className="info">
-            <h3>{item.product.name}</h3>
-            <span>{item.product.count} pieces, Weight: {item.product.weight}</span>
-            <p>
-            {item.product.description}
-            </p>
+    <div className="infoPage">
+      {item ? (
+        <>
+          <div className="head">
+            <div className="image">
+              <Paper elevation={3}>
+                <img src={item.product.url} alt={item.product.name} />
+              </Paper>
+            </div>
+            <Paper elevation={3}>
+              <div className="info">
+                <h3>{item.product.name}</h3>
+                <span>
+                  {item.product.count} pieces, Weight: {item.product.weight}
+                </span>
+                <p>{item.product.description}</p>
+              </div>
+            </Paper>
           </div>
-        </Paper>
-      </div>
-      <div className="commentSection">
-        <Paper elevation={3}>
-          <div className="comments">
-            <h3>Comments</h3>
-            <CommentItem />
-            <CommentItem />
-            <CommentItem />
-            <CommentItem />
+          <div className="commentSection">
+            <Paper elevation={3}>
+              <div className="comments">
+                <h3>Comments</h3>
+                {comments ? comments.map(el =>
+                <CommentItem item={el}/>
+                ) : "No comments"}
+              </div>
+            </Paper>
           </div>
-        </Paper>
-      </div>
-    </>): (<h1>No data for this product</h1>)}
+        </>
+      ) : (
+        <h1>No data for this product</h1>
+      )}
     </div>
   );
 }
